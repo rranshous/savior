006ef0c2173d9ebad3c83e7377f83f9ea9b463ec
Robby Ranshous <rranshous@gmail.com>
2015-10-14 20:14:24 -0400

    using solecist store

--- index.js
+++ index.js
@@ -1,30 +1,67 @@
 console.log('Loading function');
 
-var redis = require("redis"),
-    client = redis.createClient('redis://pub-redis-10507.us-east-1-4.6.ec2.redislabs.com:10507');
-    client.auth('bow_to_savior');
+var http = require("http");
+
+var set = function(entity_id, data, success, fail) {
+  console.log('setting',entity_id,data);
+  var base_url = "http://page-solecist.forebodingflavor.com/"
+  var view_schema = {
+    'VERSION': 1,
+    'url': 'NEW'
+  };
+  var metadata = {};
+  var post_data = {
+    'data': data,
+    'view_schema': view_schema,
+    'metadata': metadata
+  };
+  var data_string = JSON.stringify(post_data);
+  var options = {
+    hostname: 'page-solecist.forebodingflavor.com',
+    port: 80,
+    path: '/'+entity_id,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': data_string.length
+    }
+  };
+  var result;
+  var req = http.request(options, function(res) {
+    var body = '';
+    console.log('Status:', res.statusCode);
+    console.log('Headers:', JSON.stringify(res.headers));
+    res.setEncoding('utf8');
+    res.on('data', function(chunk) {
+      body += chunk;
+    });
+    res.on('end', function() {
+      console.log('Successfully processed HTTPS response');
+      // If we know it's JSON, parse it
+      if (res.headers['content-type'] === 'application/json') {
+        result = JSON.parse(body);
+      }
+      success(result);
+    });
+  });
+  req.on('error', fail);
+  req.write(data_string);
+  req.end();
+  console.log('done with set');
+}
 
 exports.handler = function(event, context) {
-    console.log('Received event:', JSON.stringify(event, null, 2));
-    console.log('context:', JSON.stringify(context, null, 2));
+  console.log('Received event:', JSON.stringify(event, null, 2));
+  console.log('context:', JSON.stringify(context, null, 2));
 
-    client.set("string key", "string val", redis.print);
-    client.hset("hash key", "hashtest 1", "some value", redis.print);
-    client.hset(["hash key", "hashtest 2", "some other value"], redis.print);
-    client.hkeys("hash key", function (err, replies) {
-        console.log(replies.length + " replies:");
-        replies.forEach(function (reply, i) {
-            console.log("    " + i + ": " + reply);
-        });
-        client.quit();
-    });
+  var page_url = event.page_url;
+  delete event.page_url;
+  console.log('page_url',page_url);
 
-    var page_url = event.page_url;
-    delete event.page_url;
-
-    client.lpush('all_page_urls', page_url);
-    client.hset('page`'+page_url, 'url', page_url);
-
-    console.log('page_url',page_url);
-    context.succeed('v1; page_url: ' + page_url);
+  console.log('setting keys');
+  var page_url_encoded = new Buffer(page_url).toString('base64');
+  set(page_url_encoded, { url: page_url }, function() {
+    console.log('done setting keys');
+    context.succeed('v6; page_url: ' + page_url);
+  }, context.fail);
 };
